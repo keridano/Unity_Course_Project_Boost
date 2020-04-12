70b404202806b13f3d2baf53f43df35f5d347734
Enrico <keridano@gmail.com>
2020-04-12 19:06:32 +0200

    Rocket Thrust and rotation

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource rocketThrust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rocketThrust = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!rocketThrust.isPlaying)
+                rocketThrust.Play();
+        }
+        else
+        {
+            if (rocketThrust.isPlaying)
+                rocketThrust.Stop();
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+    }
+}
