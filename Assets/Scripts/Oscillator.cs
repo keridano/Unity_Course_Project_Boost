1b43d3af522977bcda25bf55eaf75625ddab3d3c
Enrico <keridano@gmail.com>
2020-04-14 15:01:09 +0200

    First automatic obscillation implemented

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 2f;
+
+    //[Range(0, 1)] [SerializeField] float movementFactor;
+
+    Vector3 startingPos;
+
+    const float tau = Mathf.PI * 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cycles = Time.time / period;
+        var rawSinWave = Mathf.Sin(cycles * tau);
+          
+        var offset = movementVector * rawSinWave;
+        transform.position = startingPos + offset;
+    }
+}
