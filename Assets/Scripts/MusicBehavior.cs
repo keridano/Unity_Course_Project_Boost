f608ebacee705cbb3878195e083b0582fef9aef6
Enrico <keridano@gmail.com>
2020-04-18 18:44:36 +0200

    Music now loops through the whole game

---
+++ Assets/Scripts/MusicBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicBehavior : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int maxIndex = SceneManager.sceneCountInBuildSettings - 1;
+
+        if (currentIndex == 0)
+        {
+            if (objs.Length > 1)
+                Destroy(objs[1]);
+
+            DontDestroyOnLoad(objs[0]);
+        }
+        else if(currentIndex == maxIndex)
+        {
+            if (objs.Length > 1)
+                Destroy(objs[0]);
+        }
+    }
+}
